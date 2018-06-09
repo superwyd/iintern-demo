4d82c24266902dc624978b8d48d8d029f91cf69d
superwyd <halawyd@gmail.com>
2018-06-08 22:12:09 -0400

    init

---
+++ js/main.js
@@ -0,0 +1,73 @@
+var vid = document.getElementById("bgvid");
+var pauseButton = document.querySelector("#polina button");
+
+// ------------scroll smooth
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("#contact-btn").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+
+var options = {
+	  useEasing : false,
+	  useGrouping : true,
+	  separator : ',',
+	  decimal : '.',
+	  prefix : '',
+	  suffix : ''
+};
+var $myNumber=8878,//要变化数字
+    mydemo = new CountUp("myElementId", 0, $myNumber, 0, 1.5, options);
+mydemo.start();
+
+
+if (window.matchMedia('(prefers-reduced-motion)').matches) {
+    vid.removeAttribute("autoplay");
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
+}
+
+function vidFade() {
+  vid.classList.add("stopfade");
+}
+
+vid.addEventListener('ended', function()
+{
+// only functional if "loop" is removed 
+vid.pause();
+// to capture IE10
+vidFade();
+}); 
+
+
+pauseButton.addEventListener("click", function() {
+  vid.classList.toggle("stopfade");
+  if (vid.paused) {
+    vid.play();
+    pauseButton.innerHTML = "Pause";
+  } else {
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
+  }
+})
+
