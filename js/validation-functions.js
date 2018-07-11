0275917f4785c45d89380abba175a6087331faa2
superwyd <halawyd@gmail.com>
2018-07-11 18:19:21 -0400

    update intern apply form and home video

---
+++ js/validation-functions.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+    $('.test-form').bootstrapValidator({
+        //submitButtons: '#postForm',
+        // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later      
+        fields: {
+            documentEmail: {
+                validators: {
+                    notEmpty: {
+                        message: 'The email address is required and cannot be empty'
+                    },
+                    emailAddress: {
+                        message: 'The email address is not a valid'
+                    }
+                }
+            },
+            documentNote: {
+                message: 'Address is not valid',
+                validators: {
+                    notEmpty: {
+                        message: 'Address is required and cannot be empty'
+                    }
+                }
+            }, 
+
+        }
+    })
+    .on('success.form.bv', function(e) {
+        // Prevent form submission
+        e.preventDefault();
+
+        // Get the form instance
+        var $form = $(e.target);
+
+        // Get the BootstrapValidator instance
+        var bv = $form.data('bootstrapValidator');
+
+        // Use Ajax to submit form data
+        var url = 'https://script.google.com/macros/s/AKfycbyp7JeHnm_Jh5J2dqZkbigouJPBjHPeBBqsAFKyKMqYI157kFGM/exec';
+        var redirectUrl = 'success-page.html';
+        // show the loading 
+        $('.submit-form').prepend($('<span></span>').addClass('glyphicon glyphicon-refresh glyphicon-refresh-animate'));
+        console.log($form.serialize());
+        var jqxhr = $.post(url, $form.serialize(), function(data) {
+            console.log("Success! Data: " + data.statusText);
+            $(location).attr('href',redirectUrl);
+        })
+            .fail(function(data) {
+                console.warn("Error! Data: " + data.statusText);
+                // HACK - check if browser is Safari - and redirect even if fail b/c we know the form submits.
+                if (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0) {
+                    //alert("Browser is Safari -- we get an error, but the form still submits -- continue.");
+                    $(location).attr('href',redirectUrl);                
+                }
+            });
+    });
+});
